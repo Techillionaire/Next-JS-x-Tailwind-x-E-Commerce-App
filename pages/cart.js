ba476c56b580ebf02c49101ef8a3d3521d76785e
Techillionaire <techillionaire@gmail.com>
2022-09-20 13:03:26 +0100

    -- Login page & validation feature done

--- pages/cart.js
+++ pages/cart.js
@@ -100,7 +100,7 @@ function CartScreen(){
                                 </div>
                             </li>
                             <li>
-                                <button onClick={() => router.push('/shipping')} className='primary-button w-full'>Check out</button>
+                                <button onClick={() => router.push('login?redirect=/shipping')} className='primary-button w-full'>Check out</button>
                             </li>
                         </ul>
                     </div>
---
+++ pages/login.js
@@ -0,0 +1,74 @@
+import Link from 'next/link'
+import React from 'react'
+import Layout from '../components/Layout'
+import {useForm} from 'react-hook-form'
+
+const LoginScreen = () => {
+
+    const {
+            handleSubmit,
+            register,
+            formState: { errors },
+        } = useForm();
+    
+    const submitHandler = ({ email, password }) => {
+        console.log(email, password);
+    }
+
+  return (
+    <Layout title={`login`}>
+        <form className='mx-auto max-w-screen-sm' onSubmit={handleSubmit(submitHandler)}>
+            <h1 className='mb-4 text-xl font-bold text-center'>Login</h1>
+            <div className='mb-4'>
+                <label className='' htmlFor='email'>Email</label>
+                <input 
+                    type='email' 
+                    {...register('email', {
+                        required: 'Please enter email',
+                        pattern: {
+                          value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/i,
+                          message: 'Please enter valid email',
+                        },
+                      })}
+                    id='email' 
+                    className='w-full' 
+                    autoFocus 
+                    placeholder='Enter your email' 
+                />
+                {errors.email && (
+                    <div className="text-red-500">{errors.email.message}</div>
+                )}
+            </div>
+            
+            <div className='mb-4'>
+                <label className='' htmlFor='password'>Password</label>
+                <input 
+                    type="password"
+                    {...register('password', {
+                      required: 'Please enter password',
+                      minLength: { value: 6, message: 'password should be more than 5 characters' },
+                    })}
+                    id='password' 
+                    className='w-full' 
+                    autoFocus 
+                    placeholder='Enter your password' 
+                />
+                 {errors.password && (
+                    <div className="text-red-500 ">{errors.password.message}</div>
+                )}
+            </div>
+            
+            <div className='mb-4'>
+                <button className='primary-button w-full'>Login</button>
+            </div>
+
+            <div className='mb-4 text-center'>
+                Don&apos;t have an account? &nbsp;
+                <Link href={`register`}><a>Register</a></Link>
+            </div>
+        </form>
+    </Layout>
+  )
+}
+
+export default LoginScreen
